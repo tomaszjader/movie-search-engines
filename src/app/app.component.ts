cc680d44cd9478411334c11bd36a4512f667debd
Tomasz Jąder <tomasz.jader@gmail.com>
2025-04-13 11:53:44 +0200

    added movie-card-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SearchEnginesComponent } from './components/search-engines/search-engines.component';
+import { MovieCardComponent } from "./components/movie-card/movie-card.component";
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, SearchEnginesComponent],
+    imports: [RouterOutlet, SearchEnginesComponent, MovieCardComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
---
+++ src/app/components/movie-card/movie-card.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+
+@Component({
+  selector: 'app-movie-card',
+  imports: [MatCardModule],
+  templateUrl: './movie-card.component.html',
+  styleUrl: './movie-card.component.scss'
+})
+export class MovieCardComponent {
+  card = {
+    "Title": "Batman: Mask of the Phantasm",
+    "Year": "1993",
+    "imdbID": "tt0106364",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BYjYwN2ZmZWYtMTdlNC00ZDQxLWEyNmUtZDI5Yjk5MzQ2N2Y3XkEyXkFqcGc@._V1_SX300.jpg"
+  };
+
+}
