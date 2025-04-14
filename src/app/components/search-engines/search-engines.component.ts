77ad864118fdbcbf43e00f1309299dc300a2f326
Tomasz Jąder <tomasz.jader@gmail.com>
2025-04-14 23:49:55 +0200

    added input

--- src/app/components/search-engines/search-engines.component.ts
+++ src/app/components/search-engines/search-engines.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-search-engines',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './search-engines.component.html',
   styleUrl: './search-engines.component.scss'
 })
 export class SearchEnginesComponent {
+  search(){
 
+  }
+  searchText = '';
+  searchResults: any[] = [];
 }
